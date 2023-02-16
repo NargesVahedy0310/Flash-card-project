46763a2b270f26bfc2feaa0038d8ddab7c44debd
narges <nargesvahedy0310@gmail.com>
2023-02-16 12:35:49 +0330

    edit name

--- blog/views.py
+++ blog/views.py
@@ -10,18 +10,18 @@ class BlogMe(APIView):
         count = len(ip_address)
         all_ip = []
         for num in range(count):
+
             ip_ad = ViewSite.objects.all().values()[::-1][num]
             ip = ip_ad['ip_address']
             all_ip.append(ip)
         hostname = socket.gethostname()
         ip_address = socket.gethostbyname(hostname)
         if not ip_address in all_ip:
-            ip = ViewSite.objects.create(hostname=hostname, ip_address=ip_address)
+            my_ip = ViewSite.objects.create(hostname=hostname, ip_address=ip_address)
 
         article = Article.objects.get(id=2)
         article.views = len(all_ip)
         article.save()
-
         Articles = Article.objects.all()
         context = {'data': Articles}
         return render(request, 'blogs.html', context)
